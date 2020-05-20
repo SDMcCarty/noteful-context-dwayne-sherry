bd4808e5ca200e00efc89ab7c240fcd95e4e28ef
Sherry McCarty <sherrydmccarty@gmail.com>
2020-05-20 16:44:59 -0400

    added delete button with redirect

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,70 @@
 import React, {Component} from 'react';
-import {Route, Link} from 'react-router-dom';
+import {Route, Link, Redirect } from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import NoteListNav from '../NoteListNav/NoteListNav';
 import NotePageNav from '../NotePageNav/NotePageNav';
 import NoteListMain from '../NoteListMain/NoteListMain';
 import NotePageMain from '../NotePageMain/NotePageMain';
-import dummyStore from '../dummy-store';
 import {getNotesForFolder, findNote, findFolder} from '../notes-helpers';
 import './App.css';
+import NotefulContext from '../NotefulContext';
 
 class App extends Component {
-    state = {
-        notes: [],
-        folders: []
-    };
+    constructor(props) {
+        super(props)
+        this.state = {
+            notes: [],
+            folders: []
+        };
+    }
+    
+    
+    componentDidMount = () => {
+        let newNotes = []; //1
+        let newFolders = []; //2
+        fetch('http://localhost:9090/notes') //4
+            .then(res => {
+                if(!res.ok) { //4.1
+                    throw new Error(res.status); //4.2
+                } return res.json() //4.2
+                }).then (data => { 
+                    this.setState({ notes: data }) //4.4
+                }).catch(err => 
+                    console.log(err));
 
-    componentDidMount() {
-        // fake date loading from API call
-        setTimeout(() => this.setState(dummyStore), 600);
+        fetch('http://localhost:9090/folders') //5
+            .then(res => {
+                if(!res.ok) { //5.1
+                    throw new Error(res.status); //5.2
+                } return res.json() //5.2
+            }).then(data => {
+                this.setState({ folders: data }) //5.4
+            }).catch(err => 
+                console.log(err));
+
+        this.setState({
+            notes: newNotes,
+            folders: newFolders
+        }) //4.01/5.01
+        
+    }
+
+    deleteNote = (id) => {
+        fetch(`http://localhost:9090/notes/${id}`, {
+            method: 'DELETE',
+            headers: {
+              'content-type': 'application/json'
+            },
+          }).then(res => {
+              if(!res.ok) {
+                throw new Error('Something went wrong')
+              } return res.json()
+          }).then(data => {
+              this.setState({
+                  notes: this.state.notes.filter(note => note.id !== id)
+              })
+          })
+
     }
 
     renderNavRoutes() {
@@ -30,11 +77,7 @@ class App extends Component {
                         key={path}
                         path={path}
                         render={routeProps => (
-                            <NoteListNav
-                                folders={folders}
-                                notes={notes}
-                                {...routeProps}
-                            />
+                            <NoteListNav />
                         )}
                     />
                 ))}
@@ -44,7 +87,8 @@ class App extends Component {
                         const {noteId} = routeProps.match.params;
                         const note = findNote(notes, noteId) || {};
                         const folder = findFolder(folders, note.folderId);
-                        return <NotePageNav {...routeProps} folder={folder} />;
+                        return <NotePageNav  {...routeProps} folder={folder}/>;
+                         
                     }}
                 />
                 <Route path="/add-folder" component={NotePageNav} />
@@ -54,7 +98,7 @@ class App extends Component {
     }
 
     renderMainRoutes() {
-        const {notes, folders} = this.state;
+        const { notes } = this.state;
         return (
             <>
                 {['/', '/folder/:folderId'].map(path => (
@@ -82,7 +126,9 @@ class App extends Component {
                     render={routeProps => {
                         const {noteId} = routeProps.match.params;
                         const note = findNote(notes, noteId);
-                        return <NotePageMain {...routeProps} note={note} />;
+                        if(!note) {
+                            return <Redirect to='/' />
+                        } return <NotePageMain {...routeProps} note={note} />;
                     }}
                 />
             </>
@@ -91,7 +137,12 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
+          <NotefulContext.Provider value={{
+              notes: this.state.notes,
+              folders: this.state.folders,
+              deleteNote: this.deleteNote
+          }}>
+             <div className="App">
                 <nav className="App__nav">{this.renderNavRoutes()}</nav>
                 <header className="App__header">
                     <h1>
@@ -101,6 +152,7 @@ class App extends Component {
                 </header>
                 <main className="App__main">{this.renderMainRoutes()}</main>
             </div>
+          </NotefulContext.Provider>
         );
     }
 }
--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,31 +1,38 @@
 import React from 'react'
+import NotefulContext from '../NotefulContext';
 import { Link } from 'react-router-dom'
 import { format } from 'date-fns'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Note.css'
 
-export default function Note(props) {
-  return (
-    <div className='Note'>
-      <h2 className='Note__title'>
-        <Link to={`/note/${props.id}`}>
-          {props.name}
-        </Link>
-      </h2>
-      <button className='Note__delete' type='button'>
-        <FontAwesomeIcon icon='trash-alt' />
-        {' '}
-        remove
-      </button>
-      <div className='Note__dates'>
-        <div className='Note__dates-modified'>
-          Modified
+export default class Note extends React.Component {
+  static contextType = NotefulContext;
+
+  render() {
+    const { deleteNote } = this.context;
+    return (
+      <div className='Note'>
+        <h2 className='Note__title'>
+          <Link to={`/note/${this.props.id}`}>
+            {this.props.name}
+          </Link>
+        </h2>
+        <button className='Note__delete' type='button' onClick={() => deleteNote(this.props.id)}>
+          <FontAwesomeIcon icon='trash-alt' />
           {' '}
-          <span className='Date'>
-            {format(props.modified, 'Do MMM YYYY')}
-          </span>
+          remove
+        </button>
+        <div className='Note__dates'>
+          <div className='Note__dates-modified'>
+            Modified
+            {' '}
+            <span className='Date'>
+              {format(this.props.modified, 'Do MMM YYYY')}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+ 
 }
